d1c11440b39a5c2d46f1fa11d12b69bf7275d629
Nathaniel Kerr <nathanielkerr10@gmail.com>
2020-07-10 00:51:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,7 +29,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  names = ["jake","fin","mat"]
+  names = ["am","jake","fin","mat"]
   names [0]
 end
 
